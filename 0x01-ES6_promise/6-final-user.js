e9b8619e96baf2f7c5b35ca0c1d787b4bbe78676
Anthony Osigbe <thronesofglory@gmail.com>
2024-04-10 13:27:45 +0000

    6. Handle multiple promises tasks completed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,22 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const res = [];
-
-  try {
-    const userPromise = signUpUser(firstName, lastName);
-    const uploadPromise = uploadPhoto(fileName);
-
-    const [user, upload] = await Promise.all([userPromise, uploadPromise]);
-
-    res.push({ status: 'fulfilled', value: user });
-    res.push({ status: 'fulfilled', value: upload });
-  } catch (err) {
-    res.push({
-      status: 'rejected',
-      value: err.toString(),
-    });
-  }
-
-  return res;
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }

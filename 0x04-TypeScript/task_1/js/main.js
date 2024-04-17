be0756b4be03b11f93992aef9a294e21e63f1f7d
Anthony Osigbe <thronesofglory@gmail.com>
2024-04-17 21:37:11 +0100

    1. Let's build a Teacher interface tasks completed

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,25 @@
+"use strict";
+exports.__esModule = true;
+exports.createStudent = exports.StudentClass = exports.printTeacher = void 0;
+function printTeacher(firstName, lastName) {
+    return "".concat(firstName[0], ". ").concat(lastName);
+}
+exports.printTeacher = printTeacher;
+var StudentClass = /** @class */ (function () {
+    function StudentClass(firstName, lastName) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+    StudentClass.prototype.workOnHomework = function () {
+        return 'Currently working';
+    };
+    StudentClass.prototype.displayName = function () {
+        return this._firstName;
+    };
+    return StudentClass;
+}());
+exports.StudentClass = StudentClass;
+function createStudent(ctor, firstName, lastName) {
+    return new ctor(firstName, lastName);
+}
+exports.createStudent = createStudent;
